7adddebc3e0a05743bd694824145e30a2994fb38
Andrey <akozlov@rocketmail.com>
2023-04-02 01:20:30 +0300

    working feedback screen , but without routes. Adding and deleting feedbacks

---
+++ feedback-app/src/App.js
@@ -0,0 +1,40 @@
+import {v4 as uuidv4} from 'uuid'
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackData from './data/FeedbackData'
+import FeedbackForm from "./components/FeedbackForm"
+
+
+function App() {
+
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback( feedback.filter( (item) => item.id !== id ) )
+        }
+    }
+
+    const addFeedback = (newFeedback) => {
+       newFeedback.id = uuidv4()
+       setFeedback([newFeedback, ...feedback])
+    }
+
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackForm handleAdd={addFeedback} />
+               <FeedbackStats feedback= {feedback}/>
+               <FeedbackList feedback= {feedback} 
+               handleDelete = {deleteFeedback}
+               />
+            </div>
+        </>
+
+    )
+}
+
+export default App
